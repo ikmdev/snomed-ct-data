af3214fc631f463ae69df0c42560378f32e465a1
Jonathan Romero <141253593+jonaromero86@users.noreply.github.com>
2024-11-25 13:37:27 -0500

    Adding method assertConcept()

--- snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/ConceptSemanticIT.java
+++ snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/ConceptSemanticIT.java
@@ -6,15 +6,27 @@ import dev.ikm.tinkar.common.service.PrimitiveData;
 import dev.ikm.tinkar.common.service.ServiceKeys;
 import dev.ikm.tinkar.common.service.ServiceProperties;
 import dev.ikm.tinkar.common.util.uuid.UuidUtil;
+import dev.ikm.tinkar.component.Component;
+import dev.ikm.tinkar.coordinate.stamp.StampCoordinateRecord;
+import dev.ikm.tinkar.coordinate.stamp.StampPositionRecord;
 import dev.ikm.tinkar.coordinate.stamp.StateSet;
+import dev.ikm.tinkar.coordinate.stamp.calculator.Latest;
+import dev.ikm.tinkar.coordinate.stamp.calculator.StampCalculator;
+import dev.ikm.tinkar.entity.EntityService;
+import dev.ikm.tinkar.entity.PatternEntityVersion;
+import dev.ikm.tinkar.entity.SemanticRecord;
+import dev.ikm.tinkar.entity.SemanticVersionRecord;
 import dev.ikm.tinkar.terms.EntityProxy;
+import dev.ikm.tinkar.terms.TinkarTerm;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -33,11 +45,19 @@ public class ConceptSemanticIT {
         PrimitiveData.stop();
     }
 
+    /**
+     * Test Concepts Semantics.
+     *
+     * @result Reads content from file and validates Concept of Semantics by calling private method assertConcept().
+     */
     @Test
     public void testConceptSemantics() throws IOException {
         String sourceFilePath = System.getProperty("user.home") + "/data/SnomedCT_InternationalRF2_PRODUCTION_20240201T120000Z/Full/Terminology/sct2_Concept_Full_INT_20240201.txt";
+        String errorFile = "target/failsafe-reports/concepts_not_found.txt";
+        int notFound = 0;
 
-        try (BufferedReader br = new BufferedReader(new FileReader(sourceFilePath))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(sourceFilePath));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(errorFile))) {
             String line;
             while ((line = br.readLine()) != null) {
                 if (line.startsWith("id")) continue;
@@ -50,8 +70,33 @@ public class ConceptSemanticIT {
                 EntityProxy.Concept moduleId = EntityProxy.Concept.make(PublicIds.of(UuidUtil.fromSNOMED(columns[3])));
                 EntityProxy.Concept definitionStatus = EntityProxy.Concept.make(PublicIds.of(UuidUtil.fromSNOMED(columns[4])));
 
+//                if (!assertConcept(id, term, descriptionType, caseSensitivityConcept, effectiveTime, descriptionStatus)) {
+//                    notFound++;
+//                    bw.write(term + "\t" + id + "\t" + columns[1] +
+//                            "\t" + (descriptionStatus.equals(StateSet.ACTIVE) ? "Active" : "Inactive") +
+//                            "\t" + descriptionType.description() +
+//                            "\t" + caseSensitivityConcept.description() + "\n");
+//                }
+
             }
         }
+    }
+
+    private boolean assertConcept(UUID id, String term, EntityProxy.Concept nameType, EntityProxy.Concept caseSensitive, long effectiveDate, StateSet activeFlag) {
+        StampPositionRecord stampPosition = StampPositionRecord.make(effectiveDate, TinkarTerm.DEVELOPMENT_PATH.nid());
+        StampCalculator stampCalc = StampCoordinateRecord.make(activeFlag, stampPosition).stampCalculator();
+        SemanticRecord entity = EntityService.get().getEntityFast(id);
+
+        PatternEntityVersion latestDescriptionPattern = (PatternEntityVersion) stampCalc.latest(TinkarTerm.DESCRIPTION_PATTERN).get();
+        Latest<SemanticVersionRecord> latest = stampCalc.latest(entity);
+        if (latest.isPresent()) {
+            Component descriptionType = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.DESCRIPTION_TYPE, latest.get());
+            Component caseSensitivity = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.DESCRIPTION_CASE_SIGNIFICANCE, latest.get());
+            String text = latestDescriptionPattern.getFieldWithMeaning(TinkarTerm.TEXT_FOR_DESCRIPTION, latest.get());
+
+            return descriptionType.equals(nameType) && caseSensitivity.equals(caseSensitive) && text.equals(term);
+        }
+        return false;
     }
 
 }
