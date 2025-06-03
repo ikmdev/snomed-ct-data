ab74fe14a0b4ff29e85c628a10765907e2756a88
Greg Liebowitz <gliebowitz@deloitte.com>
2025-06-03 11:36:21 -0400

    IKDT-923 Formatting

--- snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/SnomedElkSnomedCompareTestIT.java
+++ snomed-ct-integration/src/test/java/dev/ikm/tinkar/snomedct/integration/SnomedElkSnomedCompareTestIT.java
@@ -17,60 +17,60 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SnomedElkSnomedCompareTestIT extends ElkSnomedClassifierTestBase implements SnomedVersion {
 
-	private static final Logger LOG = LoggerFactory.getLogger(SnomedElkSnomedCompareTestIT.class);
+    private static final Logger LOG = LoggerFactory.getLogger(SnomedElkSnomedCompareTestIT.class);
 
-	private static Path origin;
+    private static Path origin;
 
-	@BeforeAll
-	public static void startPrimitiveData() {
-		origin = IntegrationTestUtils.findOriginPath("SnomedCT_").resolve("Snapshot", "Terminology");
-		File datastorePath = new File(System.getProperty("datastorePath"));
-		LOG.info("datastorePath: {}", datastorePath);
-		ServiceProperties.set(ServiceKeys.DATA_STORE_ROOT, datastorePath);
-		PrimitiveData.selectControllerByName("Open SpinedArrayStore");
-		PrimitiveData.start();
-	}
+    @BeforeAll
+    public static void startPrimitiveData() {
+        origin = IntegrationTestUtils.findOriginPath("SnomedCT_").resolve("Snapshot", "Terminology");
+        File datastorePath = new File(System.getProperty("datastorePath"));
+        LOG.info("datastorePath: {}", datastorePath);
+        ServiceProperties.set(ServiceKeys.DATA_STORE_ROOT, datastorePath);
+        PrimitiveData.selectControllerByName("Open SpinedArrayStore");
+        PrimitiveData.start();
+    }
 
-	@AfterAll
-	public static void stopPrimitiveData() {
-		LOG.info("stopPrimitiveData");
-		PrimitiveData.stop();
-		LOG.info("Stopped");
-	}
+    @AfterAll
+    public static void stopPrimitiveData() {
+        LOG.info("stopPrimitiveData");
+        PrimitiveData.stop();
+        LOG.info("Stopped");
+    }
 
-	@Override
-	public String getDir() {
-		return origin.toFile().getPath();
-	}
+    @Override
+    public String getDir() {
+        return origin.toFile().getPath();
+    }
 
-	@Override
-	public String getEdition() {
-		return IntegrationTestUtils.findEditionFromOrigin(origin);
-	}
+    @Override
+    public String getEdition() {
+        return IntegrationTestUtils.findEditionFromOrigin(origin);
+    }
 
-	@Override
-	public String getEditionDir() {
-		return ""; // Not used
-	}
+    @Override
+    public String getEditionDir() {
+        return ""; // Not used
+    }
 
-	@Override
-	public String getVersion() {
-		return IntegrationTestUtils.findVersionFromOrigin(origin);
-	}
+    @Override
+    public String getVersion() {
+        return IntegrationTestUtils.findVersionFromOrigin(origin);
+    }
 
-	@Override
-	public String getInternationalVersion() {
-		return getVersion();
-	}
+    @Override
+    public String getInternationalVersion() {
+        return getVersion();
+    }
 
-	@Override
-	public void versionDataFile() {
-		assertTrue(true);
-	}
+    @Override
+    public void versionDataFile() {
+        assertTrue(true);
+    }
 
-	@Override
-	public void versionClass() {
-		assertTrue(true);
-	}
+    @Override
+    public void versionClass() {
+        assertTrue(true);
+    }
 
 }
